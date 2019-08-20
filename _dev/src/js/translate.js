476dc712d635248f789e7d02b98aff5750eb5465
Hondo <hi@hondo.co>
2019-08-20 07:09:38 -0400

    Fix translations on Chrome

--- _dev/src/js/translate.js
+++ _dev/src/js/translate.js
@@ -1,46 +1,40 @@
 const Cookies = require('js-cookie');
 
 const googleTranslateConfig = {
-    lang: "en"
+    lang: "en",
+    domain: "ctoec.org"
 };
 
 window.TranslateInit = function() {
     let code = TranslateGetCode();
-    let domain = document.domain;
 
     if (code == googleTranslateConfig.lang) {
-        TranslateCookieHandler(null, domain);
+        TranslateCookieHandler(null, googleTranslateConfig.domain);
     }
 
     new google.translate.TranslateElement({
         pageLanguage: googleTranslateConfig.lang,
     });
 
-    TranslateEventHandler('click', '[data-google-lang]', function (e, el) {
-        e.preventDefault();
-
-        if (el.getAttribute("data-google-lang") !== 'en') {
-            TranslateCookieHandler("/" + googleTranslateConfig.lang + "/" + el.getAttribute("data-google-lang"), domain);
-        } else {
-            Cookies.set('googtrans', null);
-        }
-
+    TranslateEventHandler('click', '[data-google-lang]', function (e) {
+        TranslateCookieHandler("/" + googleTranslateConfig.lang + "/" + e.target.getAttribute("data-google-lang"), googleTranslateConfig.domain);
         window.location.reload();
     });
 }
 
 function TranslateGetCode() {
     let lang = (Cookies.get('googtrans') != undefined && Cookies.get('googtrans') != "null") ? Cookies.get('googtrans') : googleTranslateConfig.lang;
-    return lang.substr(-2);
-} 
+    return lang.match(/(?!^\/)[^\/]*$/gm)[0];
+}
 
 function TranslateCookieHandler(val, domain) {
     Cookies.set('googtrans', val);
     Cookies.set("googtrans", val, {
-        domain: "." + domain,
+        domain: "." + document.domain,
     });
 
     if (domain == "undefined") return;
+    
     Cookies.set("googtrans", val, {
         domain: domain,
     });
@@ -51,11 +45,7 @@ function TranslateCookieHandler(val, domain) {
 }
 
 function TranslateEventHandler(event, el, handler) {
-    let translateLinks = document.querySelectorAll('[data-google-lang]');
-
-    translateLinks.forEach(function(link) {
-        link.addEventListener('click', (e) => {
-            handler(e, link);
-        });
-    })
+    document.addEventListener(event, function (e) {
+        if (e.target.closest(el)) handler(e);
+    });
 }
