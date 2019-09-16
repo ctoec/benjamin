ab77f011d39f8c9115cefba2c7f2986f01fc3d74
Matthew Crist <hello@matthewcrist.com>
2019-09-16 19:25:49 -0400

    Cleans up hear

--- blocks/home-page-hero/block.php
+++ blocks/home-page-hero/block.php
@@ -4,15 +4,17 @@
     $background = block_value( 'background-image' );
 ?>
 
-<div class="b b--hero padding-y-8" style="background-image: url(<?php echo wp_get_attachment_url( $background ); ?>)">
-    <div class="grid-container">
-        <div class="grid-row">
-            <?php if ($logo) { ?>
-                <img src="<?php echo wp_get_attachment_url( $logo ); ?>" alt="<?php echo $heading; ?>" class="height-6" />
-            <?php } ?>
+<div class="b b--hero--image" style="background-image: url(<?php echo wp_get_attachment_url( $background ); ?>)">
+    <div class="b--hero padding-y-8" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/frontend/img/hero-bg.svg'; ?>)">
+        <div class="grid-container">
+            <div class="grid-row">
+                <?php if ($logo) { ?>
+                    <img src="<?php echo wp_get_attachment_url( $logo ); ?>" alt="<?php echo $heading; ?>" class="height-6" />
+                <?php } ?>
+            </div>
+            <h1 class="font-sans-3xl text-white line-height-sans-3 text-normal margin-bottom-0 maxw-tablet">
+                <?php echo $heading; ?>
+            </h1>
         </div>
-        <h1 class="font-sans-3xl text-white line-height-sans-3 text-normal margin-bottom-0 maxw-tablet">
-            <?php echo $heading; ?>
-        </h1>
     </div>
 </div>
