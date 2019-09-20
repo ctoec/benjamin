3f8066fb637ef77683f2f3668834d403cdbba9a0
Matthew Crist <hello@matthewcrist.com>
2019-09-20 13:49:57 -0400

    Update navbar styling

--- inc/frontend/navbars/navbar.php
+++ inc/frontend/navbars/navbar.php
@@ -12,13 +12,13 @@ $sticky = get_theme_mod('navbar_sticky_setting', 'no') == 'yes' ? 'sticky' : '';
       <button class="usa-menu-btn">Menu</button>
       <nav role="navigation" class="usa-nav display-flex flex-align-center flex-fill">
         <button class="usa-nav__close"><img src="<?php echo get_template_directory_uri() ?>/assets/frontend/img/close.svg" alt="close"></button>
-        <div class="desktop:order-last display-flex desktop:flex-fill flex-align-center desktop:flex-justify-end">
+        <div class="desktop:order-last display-flex desktop:flex-auto flex-align-center desktop:flex-justify-end">
             <div class="usa-search--cse display-flex flex-align-center">
                 <script async src="https://cse.google.com/cse.js?cx=016363712229386134106:tlizitetjji"></script>
                 <div class="gcse-searchbox-only" data-resultsUrl="/results"></div>
             </div>
         </div>
-        <div class="desktop:display-flex width-full desktop:width-auto flex-align-center">
+        <div class="desktop:display-flex width-full desktop:width-auto desktop:flex-fill flex-align-center">
             <?php
             $args =  array(
                 'theme_location' => 'primary',
@@ -30,7 +30,7 @@ $sticky = get_theme_mod('navbar_sticky_setting', 'no') == 'yes' ? 'sticky' : '';
 
             wp_nav_menu($args);
             ?>
-            <ul id="translate-navigation" class="usa-nav__primary usa-accordion notranslate" style="margin-top: 0;">
+            <ul id="translate-navigation" class="usa-nav__primary usa-accordion notranslate width-auto margin-right-05" style="margin-top: 0;">
                 <li class="usa-nav__primary-item">
                     <button class="usa-accordion__button usa-nav__link usa-nav__translate" aria-expanded="false" aria-controls="translations">
                     <?php include(get_template_directory() . '/assets/frontend/img/globe-americas.svg'); ?>
