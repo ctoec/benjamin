4e79e1274b12c66e030b9217ab80916a5d8451e0
Matthew Crist <hello@matthewcrist.com>
2019-11-12 22:54:31 -0500

    Update preview block

--- blocks/block-preview-with-links.php
+++ blocks/block-preview-with-links.php
@@ -10,12 +10,12 @@
 
 <div class="b padding-y-8<?php if($theme == 'grey') { ?> bg-gray-5<?php } ?>">
     <div class="grid-container">
-        <div class="grid-row usa-prose">
+        <div class="grid-row usa-prose flex-align-center">
             <div class="grid-col-fill order-<?php if ($isLeft) { ?>last desktop:margin-left-7<?php } else { ?>first desktop:margin-right-7<?php } ?>">
                 <h2 class="text-violet-70v font-sans-xl text-normal margin-top-0 line-height-sans-2"><?php echo $title ?></h2>
-                <div><?php echo $content ?></div>
+                <div class="margin-bottom-3"><?php echo $content ?></div>
                 <?php if ($linksHeading) { ?>
-                    <div class="margin-top-3 text-bold line-height-sans-6"><?php echo $linksHeading ?></div>
+                    <div class="text-bold line-height-sans-6"><?php echo $linksHeading ?></div>
                 <?php } ?>
                 <?php if (block_rows( 'links' )) { ?>
                     <div class="grid-row">
@@ -30,11 +30,15 @@
                                     $link_text = block_sub_value( 'link-text' );
                                     $link_url = block_sub_value( 'link-url' );
                             ?>
-                                    <li class="padding-top-3">
+                                    <li class="padding-top-1 padding-right-3">
                                         <a href="<?php echo($link_url) ?>" class="text-no-underline">
-                                            <span class="grid-row flex-align-center">
-                                                <img src="<?php echo wp_get_attachment_url( $link_icon ); ?>" alt="<?php echo $link_text; ?>" class="width-3 margin-right-1" />
-                                                <span class="grid-col-fill"><span class="text-underline hover:text-no-underline"><?php echo($link_text) ?></span><span class="text-no-underline"> →</span></span>
+                                            <span class="grid-row">
+                                                <span class="width-3 margin-right-1 padding-top-1">
+                                                    <img src="<?php echo wp_get_attachment_url( $link_icon ); ?>" alt="<?php echo $link_text; ?>" style="width: 100%; max-height: 25px" />
+                                                </span>
+                                                <span class="grid-col-fill padding-top-05">
+                                                    <span class="text-underline hover:text-no-underline"><?php echo($link_text) ?></span><span class="text-no-underline"> →</span>
+                                                </span>
                                             </span>
                                         </a>
                                     </li>
@@ -60,7 +64,7 @@
             </div>
             <div class="grid-col-4 display-none desktop:display-block order-<?php if ($isLeft) { ?>first<?php } else { ?>last<?php } ?>">
                 <?php if ($image) { ?>
-                    <img src="<?php echo wp_get_attachment_url( $image ); ?>" alt="<?php echo $title; ?>" class="maxw-mobile width-full" />
+                    <img src="<?php echo wp_get_attachment_url( $image ); ?>" alt="<?php echo $title; ?>" class="width-full" />
                 <?php } ?>
             </div>
         </div>
