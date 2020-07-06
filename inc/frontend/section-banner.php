8e52c4cadb142af0ef60cfc43f3acacb37187e09
Hondo <hi@hondo.xyz>
2020-07-06 12:59:02 -0400

    Updates header styles

--- inc/frontend/section-banner.php
+++ inc/frontend/section-banner.php
@@ -4,8 +4,8 @@ $sidebars = wp_get_sidebars_widgets();
 
 ?>
 <!-- Gov banner BEGIN -->
-<header class="usa-dark-background padding-y-2 padding-x-2 tablet:padding-x-4 text-white">
+<div class="usa-dark-background padding-y-2 padding-x-2 tablet:padding-x-4 text-white">
     <?php echo stripslashes(get_theme_mod('banner_text_setting', null));  //WPCS: xss ok. ?>
-</header>  <!-- end accordion header -->
+</div>  <!-- end accordion header -->
 
 <!-- Gov banner END -->
