62e20dc811daedc5afde0936461013c4c7d9923e
Hondo <hi@hondo.xyz>
2020-07-11 22:39:17 -0400

    Update hero image

--- blocks/home-page-hero/block.php
+++ blocks/home-page-hero/block.php
@@ -5,17 +5,16 @@
 ?>
 
 <div class="b">
-    <div class="b--hero" style="background-image: url(<?php echo wp_get_attachment_url( $background ); ?>);">
-        <div class="b--wave" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/frontend/img/bg.svg'; ?>)">
-            <div class="padding-y-5 padding-x-2 desktop:padding-x-8">
-                <div class="maxw-tablet">
-                    <?php if ($logo) { ?>
-                        <img src="<?php echo wp_get_attachment_url( $logo ); ?>" alt="<?php echo $heading; ?>" class="height-6 desktop:display-none" />
-                    <?php } ?>
-                    <h1 class="font-sans-5xl text-white line-height-sans-2 text-normal margin-top-0 margin-bottom-0">
-                        <?php echo $heading; ?>
-                    </h1>
-                </div>
+    <div class="b--hero" style="background-image: url(<?php echo wp_get_attachment_url( $background ); ?>);"></div>
+    <div class="b--wave" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/frontend/img/bg.svg'; ?>)">
+        <div class="padding-y-5 padding-x-2 desktop:padding-x-8">
+            <div class="maxw-tablet">
+                <?php if ($logo) { ?>
+                    <img src="<?php echo wp_get_attachment_url( $logo ); ?>" alt="<?php echo $heading; ?>" class="height-6 desktop:display-none" />
+                <?php } ?>
+                <h1 class="font-sans-5xl text-white line-height-sans-2 text-normal margin-top-0 margin-bottom-0">
+                    <?php echo $heading; ?>
+                </h1>
             </div>
         </div>
     </div>
