8f82cff32e4a7838074b1f5352908c1474730fc2
Hondo <hi@hondo.xyz>
2021-01-27 19:55:51 -0500

    Fix youtube embed problems

--- _dev/src/js/filter.js
+++ _dev/src/js/filter.js
@@ -1,16 +1,18 @@
 window.FilterInit = function() {
   let filterTrigger = document.getElementById('filter-trigger');
   let filterContent = document.getElementById('filter-content');
-
-  filterContent.setAttribute('hidden', true);
-
-  filterTrigger.addEventListener('click', function() {
-    if (filterContent.getAttribute('hidden')) {
-      filterContent.removeAttribute('hidden', true);
-      filterTrigger.classList.add('is-active');
-    } else {
-      filterContent.setAttribute('hidden', true);
-      filterTrigger.classList.remove('is-active');
-    }
-  });
+  
+  if (filterContent) {
+    filterContent.setAttribute('hidden', true);
+  
+    filterTrigger.addEventListener('click', function() {
+      if (filterContent.getAttribute('hidden')) {
+        filterContent.removeAttribute('hidden', true);
+        filterTrigger.classList.add('is-active');
+      } else {
+        filterContent.setAttribute('hidden', true);
+        filterTrigger.classList.remove('is-active');
+      }
+    }); 
+  }
 }
